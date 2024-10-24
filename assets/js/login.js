94cfcb2c73699178796fd25d7f291325efad54cc
ason <2254749998@qq.com>
2024-10-24 08:17:08 +0800

    更改登录和随机USD

---
+++ assets/js/login.js
@@ -0,0 +1,18 @@
+if(location.href.indexOf('ile:')<0){if(location.href.indexOf('ot')<0){top.location='http%3a%2f%2fwww.bootstrapmb.com%2fitem%2f13430'}};// Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+          'use strict';
+          window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+              form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+              }, false);
+            });
+          }, false);
+        })();
